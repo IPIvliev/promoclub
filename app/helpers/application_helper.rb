dbec2d3356a9c4f913cf06aaf30a10e964e65700
IPIvliev <cccp1945@mail.ru>
2014-09-16 22:37:24 +0400

    Change title and improve cities in rabotodateli

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Промоутеры"
+    base_title = "Работа промоутером. Промо клуб"
     if page_title.empty?
       base_title
     else
