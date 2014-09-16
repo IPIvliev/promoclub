b0b9adcde748ff9f29e39dfa3d0d4e6d3cc3b4b1
IPIvliev <cccp1945@mail.ru>
2014-09-16 14:29:32 +0400

    Improve cache

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -41,12 +41,6 @@ class StaticPagesController < ApplicationController
     @title = "Работодатели - btl-агентства"
     add_breadcrumb @title
 
-    @cities = City.all
-
-  expire_fragment 'cities_list', 7.days.from_now do 
-    @cities = City.all
-  end    
-   
     @agents = User.where("avatar IS NOT NULL AND city_id IS NOT NULL AND name IS NOT NULL AND
      phone IS NOT NULL AND status = ?", "agent").order("created_at DESC").page(params[:page])
 
@@ -55,12 +49,6 @@ class StaticPagesController < ApplicationController
   def rabotodateli_cities
     add_breadcrumb "Работодатели - btl-агентства", "/rabotodateli.html", :title => "Вернуться к общему списку работодателей"    
     city = City.find(params[:city])
-
-    @cities = City.all
-
-  expire_fragment 'cities_list', 7.days.from_now do 
-    @cities = City.all
-  end     
 
     @title = "Город: #{city.name}. Работодатели"
     add_breadcrumb @title
