427931edcd6cbffdb7ef66ec67ce4378a04e6f25
IPIvliev <cccp1945@mail.ru>
2015-01-28 15:00:50 +0300

    Change actionmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,7 +107,7 @@ Promoclub::Application.configure do
       #  domain: 'allpromoters.ru'
 #}
 
-config.action_mailer.delivery_method = :smtp
+config.action_mailer.delivery_method = :sendmail
 config.action_mailer.smtp_settings = { 
    address: "127.0.0.1", 
    port: 25, 
